e6087144bd14b52f09d4baf6f384b2b6b3e97dc8
J-P Nurmi <jp.nurmi@canonical.com>
2021-10-27 18:07:23 +0200

    Set Connect to internet as temp initial route

--- packages/ubuntu_desktop_installer/lib/installer.dart
+++ packages/ubuntu_desktop_installer/lib/installer.dart
@@ -75,7 +75,7 @@ class _UbuntuDesktopInstallerWizardState
     final service = Provider.of<DiskStorageService>(context, listen: false);
 
     return Wizard(
-      initialRoute: widget.initialRoute ?? Routes.welcome,
+      initialRoute: widget.initialRoute ?? Routes.connectToInternet,
       routes: <String, WidgetBuilder>{
         Routes.welcome: WelcomePage.create,
         // https://github.com/canonical/ubuntu-desktop-installer/issues/373
